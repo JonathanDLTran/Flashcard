2302f1fac3b0066bc0ba03af4bef52c8262c624c
Jonathan Tran <jtran2718@gmail.com>
2020-06-21 22:17:42 -0700

    add lexer keywords

---
+++ lexer.py
@@ -0,0 +1,27 @@
+# We use Jay Conrod's IMP language
+
+ASSIGN = ":="
+EQ = "="
+LT = "<"
+GT = ">"
+LTE = "<="
+GTE = ">="
+IF = "if"
+ELSE = "else"
+WHILE = "while"
+
+keywords = [
+    ASSIGN,
+    EQ,
+    LT,
+    GT,
+    LTE,
+    GTE,
+    IF,
+    ELSE,
+    WHILE,
+]
+
+
+def lex():
+    pass
